b59cf9e7dd5b0a08cb186401bb6bf4f9f2c513d0
Daniel Fernandez <daniel.fernandezgp@gmail.com>
2020-12-24 10:42:17 -0400

    Architecture created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EmployeeCreateComponent } from "./employee-create/employee-create.component";
+import { EmployeeEditComponent } from "./employee-edit/employee-edit.component";
+import { EmployeeListComponent } from "./employee-list/employee-list.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routesEmployee: Routes = [
+    { path: '', redirectTo: 'employee-list', pathMatch: 'full'},
+    { path: 'create-employee', component: EmployeeCreateComponent},
+    { path: 'employee-list', component: EmployeeListComponent},
+    { path: 'employee-edit/:id', component: EmployeeEditComponent},
+    { path: '404', component: NotFoundComponent},
+    { path: '**', redirectTo:'404'}
+]; 
+
+@NgModule({
+    imports: [RouterModule.forRoot(routesEmployee)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
